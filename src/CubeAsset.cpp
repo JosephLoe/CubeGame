2cc25f47e15646573b88a0d538b04afb05afb891
joseph <joseph_loe@hotmail.co.uk>
2013-01-04 12:43:36 +0000

    Fixed CubeAsset element buffer data.

--- src/CubeAsset.cpp
+++ src/CubeAsset.cpp
@@ -17,29 +17,29 @@ CubeAsset::CubeAsset(float x, float y, float z) {
   num_triangles = 12;
   g_vertex_buffer_data = new GLfloat[num_vertices * 3]{
 //	   X,  Y,  Z,
-	  -1, -1, -1, //1
-	   1, -1, -1, //2
-	   1,  1, -1, //3
-	  -1,  1, -1, //4
-	  -1, -1,  1, //5
-	   1, -1,  1, //6
-	   1,  1,  1, //7
-	  -1,  1,  1  //8
+	  -1, -1, -1, //0
+	   1, -1, -1, //1
+	   1,  1, -1, //2
+	  -1,  1, -1, //3
+	  -1, -1,  1, //4
+	   1, -1,  1, //5
+	   1,  1,  1, //6
+	  -1,  1,  1  //7
 
   }; // three points per vertex
   g_element_buffer_data = new GLushort[num_triangles * 3]{
 	  //linking anti clockwise
-	  0,3,1,
-	  1,1,2, //
-	  1,2,5,
-	  2,6,5, //
-	  5,6,4,
-	  4,6,7, //
-	  0,4,3,
-	  3,7,4, //
-	  0,4,1,
-	  1,4,5, //
-	  2,3,7,
+	  0,1,2,
+	  2,3,0, //
+	  0,3,4,
+	  4,7,3, //
+	  1,5,2,
+	  2,5,6, //
+	  0,4,5,
+	  5,0,1, //
+	  7,4,5,
+	  5,6,7, //
+	  3,2,7,
 	  2,7,6  //
 
   }; // three vertices per triangle
